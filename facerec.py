5f0326f9fd95328552a5f55588cc50070765b486
ferdinaafifah <52845016+ferdinaafifah@users.noreply.github.com>
2019-10-30 11:45:23 +0700

    euclidean

---
+++ facerec.py
@@ -0,0 +1,129 @@
+import cv2
+import numpy as np
+import scipy
+from scipy import spatial
+from imageio import imread
+import _pickle as pickle
+import random
+import os
+import matplotlib.pyplot as plt
+import math
+
+# Feature extractor
+def extract_features(image_path, vector_size=64):
+    image = imread(image_path)
+    try:
+        # Using KAZE, cause SIFT, ORB and other was moved to additional module
+        # which is adding addtional pain during install
+        alg = cv2.KAZE_create()
+        # Dinding image keypoints
+        kps = alg.detect(image)
+        # Getting first 32 of them. 
+        # Number of keypoints is varies depend on image size and color pallet
+        # Sorting them based on keypoint response value(bigger is better)
+        kps = sorted(kps, key=lambda x: -x.response)[:vector_size]
+        # computing descriptors vector
+        kps, dsc = alg.compute(image, kps)
+        # Flatten all of them in one big vector - our feature vector
+        dsc = dsc.flatten()
+        # Making descriptor of same size
+        # Descriptor vector size is 64
+        needed_size = (vector_size * 64)
+        if dsc.size < needed_size:
+            # if we have less the 32 descriptors then just adding zeros at the
+            # end of our feature vector
+            dsc = np.concatenate([dsc, np.zeros(needed_size - dsc.size)])
+    except cv2.error as e:
+        print ('Error: ', e)
+        return None
+
+    return dsc
+
+
+def batch_extractor(dataset_path, pickled_db_path="features.pck"):
+    dataset_files = [os.path.join(dataset_path, p) for p in sorted(os.listdir(dataset_path))]
+
+    result = {}
+    for f in dataset_files:
+        print ('Extracting features from image %s' % f)
+        name = f.split('/')[-1].lower()
+        result[name] = extract_features(f)
+    
+    print(type(result))
+
+    # saving all our feature vectors in pickled file
+    with open(pickled_db_path, 'wb') as fp:
+        pickle.dump(result, fp)
+
+
+
+class Matcher(object):
+
+    def __init__(self, pickled_db_path="features.pck"):
+        with open(pickled_db_path, 'rb') as fp:
+            self.data = pickle.load(fp)
+        self.names = []
+        self.matrix = []
+        for k, v in self.data.items():
+            self.names.append(k)
+            self.matrix.append(v)
+        self.matrix = np.array(self.matrix)
+        self.names = np.array(self.names)
+
+    def cos_cdist(self, vector):
+        # getting cosine distance between search image and images database
+        v = vector.reshape(1, -1)
+        return scipy.spatial.distance.cdist(self.matrix, v, 'euclidean').reshape(-1)
+
+    def match(self, image_path, topn=5):
+        features = extract_features(image_path)
+        img_distances = self.euclidean(features)
+        # getting top 5 records
+        nearest_ids = np.argsort(img_distances)[:topn].tolist()
+        nearest_img_paths = self.names[nearest_ids].tolist()
+        return nearest_img_paths, img_distances[nearest_ids].tolist()
+
+    def euclidean(self,test):
+        sets = self.matrix
+        v = test
+        distance = []
+        sum = 0
+        for i in sets:
+            for j in range(len(v)):
+                sum += (i[j]-v[j]) ** 2
+            d = sum ** (1/2)
+            distance.append(d)
+        return np.array(distance)
+
+
+
+def show_img(path):
+    img = imread(path, pilmode="RGB")
+    plt.imshow(img)
+    plt.show()
+    
+def run():
+    dataset_path = 'D:/IF SEM 3/ALGEO/TUBES 2/PINS/test - Copy/'
+    test_path = 'D:/IF SEM 3/ALGEO/TUBES 2/PINS/test/'
+    dataset_files = [os.path.join(dataset_path, p) for p in sorted(os.listdir(dataset_path))]
+    test_files = [os.path.join(test_path, p) for p in sorted(os.listdir(test_path))]
+    # getting 3 random images 
+    sample = random.sample(test_files, 1)
+    action = input();
+    if (action == "input"):
+        batch_extractor(dataset_path)
+    elif (action == "test"):
+        ma = Matcher('features.pck')
+        
+        for s in sample:
+            print ('Query image ==========================================')
+            show_img(s)
+            names, match = ma.match(s, topn=3)
+            print ('Result images ========================================')
+            for i in range(3):
+                # we got cosine distance, less cosine distance between vectors
+                # more they similar, thus we subtruct it from 1 to get match value
+                print ('Match %s' % (match[i]))
+                show_img(os.path.join(dataset_path, names[i]))
+
+run()
